81e53d3e1e69f5917c4668d0e23b83ea9a2a17e7
Ascendus <ascendus.dev@gmail.com>
2022-01-18 10:41:30 +1300

    feat: Added second for loop

--- main.sh
+++ main.sh
@@ -36,6 +36,11 @@ do
     echo "Looping... number $i"
 done
 
+for i in hello 1 * 2 goodbye # Where hello is a string, 1 is a number, * represents the name of first and last file in current directory, 2 is another number and goodbye is another string
+do
+    echo "Looping... i is set to $i"
+done
+
 # curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 # sudo apt-get install -y build-essential
