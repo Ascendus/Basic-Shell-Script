db40713ba183454b1c0206868ae1c833bc0fb2b3
Ascendus <ascendus.dev@gmail.com>
2022-01-18 10:46:01 +1300

    feat: Moved node/npm commands

--- main.sh
+++ main.sh
@@ -7,10 +7,3 @@ echo $TEXT
 
 echo "This is one line.
 This is another line!"
-
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -  Uncomment lines 44-46 if node and npm commands not found
-# sudo apt-get install -y nodejs                                   To install Node.js
-# sudo apt-get install -y build-essential
-
-node --version
-npm --version
---
+++ node.sh
@@ -0,0 +1,6 @@
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -  Uncomment lines 44-46 if node and npm commands not found
+# sudo apt-get install -y nodejs                                   To install Node.js
+# sudo apt-get install -y build-essential
+
+node --version
+npm --version
