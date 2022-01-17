819c78a3b2d6b38e0cc4b6227237a96f06d13d3e
Ascendus <ascendus.dev@gmail.com>
2022-01-18 10:38:36 +1300

    feat: Added loop example

--- main.sh
+++ main.sh
@@ -31,6 +31,11 @@ read FILE_NAME
 touch "${FILE_NAME}_file"
 echo "I've created a file called ${FILE_NAME} file!"
 
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    echo "Looping... number $i"
+done
+
 # curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 # sudo apt-get install -y build-essential
